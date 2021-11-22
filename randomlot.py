f780642432a53e8c3543b7b6c0f23fd016c29cde
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2021-11-22 15:01:00 +0000

    random lottery draw
    added the lottery draw and the code to match the ticket

--- randomlot.py
+++ randomlot.py
@@ -1,3 +1,8 @@
 import random
 
-your_ticket = [1, 37, 16, 28, 9, 11, 49, ]
+your_ticket = [1, 37, 16, 28, 9, 11, 49]
+lottery_arr = [random.randint(1, 50) for lottery in range(7)]
+
+winner_binary = [1 if item in your_ticket else 0 for item in lottery_arr]
+
+correct_numbers = sum(winner_binary)
