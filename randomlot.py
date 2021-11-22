8faa43adb44fd7bd36e35e6259dd54c525f450b7
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2021-11-22 15:35:59 +0000

    if else statement
    created if else statement

--- randomlot.py
+++ randomlot.py
@@ -6,3 +6,16 @@ lottery_arr = [random.randint(1, 50) for lottery in range(7)]
 winner_binary = [1 if item in your_ticket else 0 for item in lottery_arr]
 
 correct_numbers = sum(winner_binary)
+
+if correct_numbers == 3:
+    prize_money = 20
+elif correct_numbers == 4:
+    prize_money = 40
+elif correct_numbers == 5:
+    prize_money = 100
+elif correct_numbers == 6:
+    prize_money = 10000
+elif correct_numbers == 7:
+    prize_money = 100000
+else:
+    prize_money = 0
